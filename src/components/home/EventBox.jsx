5c5c1e2c4d3f57664c65c038fafc087991f02944
Hamid Eslmai <hamideslami71@gmail.com>
2024-03-05 17:27:08 +0330

    event Mouse

--- src/components/home/EventBox.jsx
+++ src/components/home/EventBox.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 export default function EventBox(props){
+
+    let styleTitleBox = {
+        backgroundColor: "gray",
+    }
+
+    const [titleStyle , setTitleStyle] = useState({});
+
+
+
     return(
         <div>
-            <h4>{props?.course?.title}</h4>
+            <h4 
+                onMouseEnter={(e) => setTitleStyle(styleTitleBox)} 
+                onMouseLeave={(e) => setTitleStyle({})} 
+                style={titleStyle}
+            >
+                {props?.course?.title}
+            </h4>
             <p>{props?.course?.description}</p>
         </div>
     )
