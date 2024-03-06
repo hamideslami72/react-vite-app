4638cd11dae7048a8164429b4ea22b1937572069
Hamid Eslmai <hamideslami71@gmail.com>
2024-03-06 15:42:02 +0330

    add todo item

--- src/components/home/TodoApp.jsx
+++ src/components/home/TodoApp.jsx
@@ -1,4 +1,4 @@
-import TodoAppTitle from "./TodoAppTitle";
+import { useState } from "react";
 import TodoListApp from "./TodoListApp";
 
 function TodoApp({todoSetting}) {
@@ -7,7 +7,7 @@ function TodoApp({todoSetting}) {
         {
             id : 1,
             title : "Tailwind CSS To DO App List 1",
-            state: false,
+            state: true,
         },
         {
             id : 2,
@@ -15,6 +15,18 @@ function TodoApp({todoSetting}) {
             state: false,
         },
     ]
+
+    const [newTodoListItem, setTodo] = useState(todoListItem) 
+
+    function addTodo(e){
+        if(e.key ==="Enter"){
+            
+            const name = e.target.name;
+            const value = e.target.value;
+            console.log(value)
+    // setInputs(values => ({...values, [name]: value}))
+        }
+    }
 
   return (
     <>
@@ -25,10 +37,12 @@ function TodoApp({todoSetting}) {
                     <h1 className="mr-6 text-4xl font-bold text-purple-600"> {todoSetting.title}</h1>
                 </div>
                 <div className="relative">
-                    <input type="text" placeholder="What needs to be done today?"
-                    className="w-full px-2 py-3 border rounded outline-none border-grey-600" />
+                    <input type="text" name="todo" placeholder="What needs to be done today?"
+                    className="w-full px-2 py-3 border rounded outline-none border-grey-600" 
+                    onKeyDown={addTodo}
+                    />
                 </div>
-                <TodoListApp key={todoListItem.id} todos={todoListItem}/>
+                <TodoListApp key={todoListItem.id} todos={newTodoListItem}/>
             </div>
         </div>
     
--- src/components/home/TodoListItem.jsx
+++ src/components/home/TodoListItem.jsx
@@ -4,8 +4,8 @@ function TodoListItem({todo}){
         <>
             <li className="relative flex items-center justify-between px-2 py-6 border-b">
             <div>
-                <input type="checkbox" onChange={() => {}} className="" />
-                <p  className="inline-block mt-1 ml-2 text-gray-600">{todo?.title}</p>
+                <input type="checkbox" checked={todo?.state} onChange={() => {}} className="" />
+                <p  className={`inline-block mt-1 ml-2 text-gray-600 ${todo?.state ? `line-through` : `` }`}>{todo?.title}</p>
             </div>
             <button type="button" className="absolute right-0 flex items-center  space-x-1">
                 <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
